e9c5e97b7db6267fbece041cc22e47a13a6ba9ea
Ange230700 <kouakouangeericstephane@gmail.com>
2025-06-03 10:56:28 +0200

    fix: 🐛  fix migration

--- prisma/migrations/20250601195407_change_price_to_string/migration.sql
+++ prisma/migrations/20250601195407_change_price_to_string/migration.sql
@@ -1,8 +1,8 @@
 /*
   Warnings:
 
-  - You are about to alter the column `price` on the `item` table. The data in that column could be lost. The data in that column will be cast from `Double` to `VarChar(191)`.
+  - You are about to alter the column `price` on the `Item` table. The data in that column could be lost. The data in that column will be cast from `Double` to `VarChar(191)`.
 
 */
 -- AlterTable
-ALTER TABLE `item` MODIFY `price` VARCHAR(191) NOT NULL;
+ALTER TABLE `Item` MODIFY `price` VARCHAR(191) NOT NULL;
